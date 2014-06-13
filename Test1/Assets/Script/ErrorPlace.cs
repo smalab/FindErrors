f8e3af8a8659220c5de0da4391b139c01789dfef
yanoty3d <vzb10326@me.com>
2014-06-13 22:50:01 +0900

    #1 マウスカーソルが入ると、ゲームオブジェクトの色が変わる。カーソルが外れてもそのまま

---
+++ Test1/Assets/Script/ErrorPlace.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ErrorPlace : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	}
+	
+	void OnMouseEnter(){
+		renderer.enabled = true;
+		renderer.material.color = Color.red;
+	}
+}
